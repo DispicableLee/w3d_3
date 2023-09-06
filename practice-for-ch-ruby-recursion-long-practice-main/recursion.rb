f7aa00fd27c3495e3e1a3e1ca936766711e97e86
DispicableLee <lee.robert053@gmail.com>
2023-09-06 14:40:19 -0400

    lol

--- practice-for-ch-ruby-recursion-long-practice-main/recursion.rb
+++ practice-for-ch-ruby-recursion-long-practice-main/recursion.rb
@@ -12,12 +12,56 @@ end
 def range(start_, end_)
     return [] if end_ < start_
     return [start_] if start_ == end_ || end_ == start_ + 1
-    [1,2]
-     new_array = []
-     new_array +[]
+
+    [start_] + range(start_+1, end_)
 end
 
-range(1,1) => [1]
-range(1,2) => [1]
-range(1,3) => range(2,3)
-    => [1,2]
+def exponentiation_vers_1(b, n)
+    return 1 if n == 0
+    b * exponentiation_vers_1(b, n-1)
+end
+
+
+# def exponentiation_vers_2(a, b)
+#     return 1 if b == 0
+#     return a if b == 1  
+#     b.times do 
+#         return a*exponentiation_vers_2(a, b-1)
+#     end
+# end
+
+# def exp
+
+def deep_dup(array)
+    temp = []
+    return array if array.empty?
+    array.each do |sub_element|
+        if sub_element.is_a?(Array)
+            temp << deep_dup(sub_array)
+        else
+            temp << sub_element
+        end
+    end
+    return temp
+
+end
+
+
+def it_fib(n)
+    fibo = [0,1,1]
+    return fibo[n-1] if n <=3
+    n.times do 
+        fibo << fibo[-1] + fibo[-2]
+    end
+    fibo[n-1]
+end
+
+def rec_fib(n)
+    return 0 if n ==1
+    return 1 if n == 2 || n == 3
+    rec_fib(n-1) + rec_fib(n-2)
+end
+
+
+
+p rec_fib(5)
